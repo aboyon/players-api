93b5e0c7be1663006b4b7183ce4585bf4f8b3f68
David Silveira <jdsilveira@gmail.com>
2015-06-21 23:21:46 -0300

    Allow CORS

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::BaseController < ActionController::API
   include ActionController::RequestForgeryProtection
   include ActionController::MimeResponds
   include ActionController::ImplicitRender
-  include ActionController::StrongParameters      
+  include ActionController::StrongParameters
+  include Api::V1::Concerns::Cors
+
   respond_to :json
   self.responder = ApiResponder
 end
---
+++ app/controllers/api/v1/concerns/cors.rb
@@ -0,0 +1,26 @@
+module Api::V1::Concerns::Cors
+  extend ActiveSupport::Concern
+
+  CORS_DOMAINS = ["http://localhost:8000/"]
+
+  included do
+    before_filter :cors_preflight_check
+    before_filter :cors_set_access_control_headers
+  end
+
+  def cors_preflight_check
+    if request.method == :options.to_s.upcase
+      cors_set_access_control_headers
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
+  def cors_set_access_control_headers
+    if CORS_DOMAINS.include? request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Allow-Origin'] = "*"
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Cache-Control, X-Prototype-Version, Content-Type, Authorization'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Max-Age'] = "1728000"
+    end
+  end
+end
