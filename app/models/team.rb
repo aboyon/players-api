8f2d19c616a6450523a37f8a33eb152d6728e7e1
David Silveira <jdsilveira@gmail.com>
2015-07-16 13:04:47 -0300

    adding players into teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,8 @@ class Team < ActiveRecord::Base
   validates_presence_of :name
   has_many :team_tournaments
   has_many :tournaments, :through => :team_tournaments
+  has_many :player_season_teams, -> { where(:active => true) }
+  has_many :players, :through => :player_season_teams
 
   def honours
     team_tournaments.where(:position => 1).map(&:tournament)
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -8,4 +8,26 @@ describe Team, type: :model do
     expect(team).to be_instance_of(Team)
   end
 
+  context "team with players" do
+
+    let(:players_contracts) {
+      [].tap { |pc|
+        16.times do |t|
+          pc << build(:player_season_team, {
+            :player   => create(:player),
+            :start_at => 1.day.ago,
+            :end_at   => DateTime.now + 1.year
+          })
+        end
+      }
+    }
+
+    it "has players" do
+      team.player_season_teams = players_contracts
+      expect(team.players).not_to be_empty
+      expect(team.players.first).to eq(players_contracts.first.player)
+    end
+  end
+
+
 end
